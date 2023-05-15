68077431f56ffdb0d3b5a305af5fa93859392672
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 13:30:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/icy_mode_dev_83415/MainActivity.java
@@ -0,0 +1,15 @@
+package com.icy_mode_dev_83415;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "icy_mode_dev_83415";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-mode-dev-83415",
+  url: "https://icy-mode-dev-83415.botics.co",
+  api: "https://icy-mode-dev-83415.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
